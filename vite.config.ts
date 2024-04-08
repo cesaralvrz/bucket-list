8b648e2d983a68949454e8af21b09018933135e9
César Álvarez <cesaralv97@gmail.com>
2024-04-08 18:48:57 +0200

    Update base on vite config file

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  base: "/bucket-list/"
+  base: "https://cesaralvrz.github.io/bucket-list/",
 })
